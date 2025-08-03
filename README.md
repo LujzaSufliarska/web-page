23bd13224bb1d1bdafbf7a6c170f4f15901711fd
xsufliarska <xsufliarska@stuba.sk>
2025-08-03 14:22:50 +0200

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Web-Portfolio
-
-My personal web page
-
----
-
-errory pri npm run deploy -> vymazat tsc -b && alebo runnut npx vite build && npx gh-pages -d dist
--- src/components/highlight/TextHighlight.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function TextHighlight({ children }: Props) {
-  return <span className="text-[var(--primary)] font-bold"> {children}</span>;
-}
