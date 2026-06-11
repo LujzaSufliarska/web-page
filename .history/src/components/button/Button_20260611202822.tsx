47ab4604f182aa4e102e1a4f94b567114293ca1c
LujzaSufliarska <lujzasuf@gmail.com>
2026-06-11 20:29:55 +0200

    CV update, button padding adjusted (cv button bug)

--- .history/src/components/button/Button_20260611202822.tsx
+++ .history/src/components/button/Button_20260611202822.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Button(props: Props) {
   return (
     <button
-      className="h-icon-m px-default py-wrapper sm:py-small text-[var(--bcg-text)] border-solid border-[var(--primary)] rounded-round border-t-1 border-l-3 border-b-3 flex gap-1 items-center cursor-pointer text-p2 sm:text-p1"
+      className="h-icon-m px-default py-small text-[var(--bcg-text)] border-solid border-[var(--primary)] rounded-round border-t-1 border-l-3 border-b-3 flex gap-1 items-center cursor-pointer text-p2 sm:text-p1"
       onClick={props.onClick}
     >
       {props.label}
